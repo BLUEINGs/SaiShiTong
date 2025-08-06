ea7c6375a3008d722d264880144878a50fef707a
BuRan <2673856803@qq.com>
2025-08-06 17:03:37 +0800

    测试提交

--- sms-server/src/main/java/com/blueing/sports_meet_system/controller/CheckController.java
+++ sms-server/src/main/java/com/blueing/sports_meet_system/controller/CheckController.java
@@ -7,19 +7,21 @@ import com.blueing.sports_meet_system.pojo.Result;
 import com.blueing.sports_meet_system.service.imp.CheckServiceA;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Slf4j
+@CrossOrigin
 @RestController
 public class CheckController {
 
     @Autowired
     private CheckServiceA checkServiceA;
 
-    @GetMapping("/chcke/noticeG")
+    @GetMapping("/saishitong/chcke/noticeG")
      public Result<Object> list(Integer smId,Integer spId){
         log.info("查询检录数据");
         Check checks = checkServiceA.listCheck(smId,spId);
--- sms-server/src/main/java/com/blueing/sports_meet_system/mapper/CheckMapper.java
+++ sms-server/src/main/java/com/blueing/sports_meet_system/mapper/CheckMapper.java
@@ -9,15 +9,14 @@ import java.util.List;
 
 @Mapper
 public interface CheckMapper {
-    @Select("select check_begin_time,check_end_time,venue from sports where sm_id = #{smId} and sp_id = #{spId}")
+
     Check listCheck(Integer smId,Integer spId);
 
-    @Select("select gid,name from sp_groups where sm_id = #{smId} and sp_id = #{spId}")
     List<Group> listGroup(Integer smId,Integer spId);
 
-    @Select("select pid,track,check_state from application_sports where sm_id = #{smId} and sp_id = #{spId} and gid= #{gid}")
+
     List<Player> listPlayer(Integer smId,Integer spId ,Integer gid);
 
-    @Select("select name,p_class from players where pid = #{pid}")
+
     Player addPlayers(Integer pid);
 }
